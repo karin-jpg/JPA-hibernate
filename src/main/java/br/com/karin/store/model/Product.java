09c4007682f390f5eb0d3c1262ebfb85d96a9c21
karin morais <andradkarin@gmail.com>
2023-03-07 16:25:00 -0300

    Created attributes for product

--- src/main/java/br/com/karin/store/model/Product.java
+++ src/main/java/br/com/karin/store/model/Product.java
@@ -1,10 +1,17 @@
 package br.com.karin.store.model;
 
+import java.math.BigDecimal;
+
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "products")
 public class Product {
 
-	
+	private Integer id;
+	private String name;
+	private String description;
+	private BigDecimal price;
 	
 }
