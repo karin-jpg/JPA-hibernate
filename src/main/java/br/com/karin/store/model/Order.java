bdf219d8fdb8ef4173a190d4c6774012851235b0
Karín Morais <andradkarin@gmail.com>
2023-03-08 12:27:41 -0300

    Added Client and Order entities

---
+++ src/main/java/br/com/karin/store/model/Order.java
@@ -0,0 +1,64 @@
+package br.com.karin.store.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private BigDecimal totalValue;
+	private LocalDate date = LocalDate.now();
+	
+	@ManyToOne
+	private Client client;
+
+	public Order() {
+	}
+
+	public Order(Client client) {
+		this.client = client;
+	}
+
+	public BigDecimal getTotalValue() {
+		return totalValue;
+	}
+
+	public void setTotalValue(BigDecimal totalValue) {
+		this.totalValue = totalValue;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
