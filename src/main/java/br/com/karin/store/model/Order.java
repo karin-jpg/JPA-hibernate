481ae616f2c48197eb050a7cb7990d9662214eae
Karín Morais <andradkarin@gmail.com>
2023-03-09 12:48:42 -0300

    Add column name specification

--- src/main/java/br/com/karin/store/model/Order.java
+++ src/main/java/br/com/karin/store/model/Order.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -21,6 +22,7 @@ public class Order {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(name = "total_value")
 	private BigDecimal totalValue;
 	private LocalDate date = LocalDate.now();
 	@ManyToOne
--- src/main/java/br/com/karin/store/model/OrderedItem.java
+++ src/main/java/br/com/karin/store/model/OrderedItem.java
@@ -2,6 +2,7 @@ package br.com.karin.store.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,6 +17,7 @@ public class OrderedItem {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(name = "unit_price")
 	private BigDecimal unitPrice;
 	private int quantity;
 	@ManyToOne
